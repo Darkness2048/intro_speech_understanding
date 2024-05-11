1a60c13e648068ffe44dc1df4ee56ecc8506cd80
Darkness2048 <153795434+Darkness2048@users.noreply.github.com>
2024-05-12 06:07:40 +0900

    Update homework4.py

--- 2024_spring/lec04/homework4.py
+++ 2024_spring/lec04/homework4.py
@@ -1,12 +1,15 @@
-
 def list_to_dict(input_list):
-    '''
-    This function should return a dictionary in which each element of 
-    `input_list` is a value, and the corresponding key is the numerical 
-    index of that element in `input_list`. 
-    '''
-    pass
+    """
+    Returns a dictionary where each element of input_list is a value,
+    and the corresponding key is the numerical index of that element in input_list.
 
+    Args:
+    input_list (list): The input list.
 
-
-
+    Returns:
+    dict: A dictionary where keys are numerical indices and values are elements from the input list.
+    """
+    result_dict = {}
+    for i in range(len(input_list)):
+        result_dict[i] = input_list[i]
+    return result_dict
